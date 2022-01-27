6b6f4974550211a7c3038af1ac10d80c4c97fe64
ClaudioPrieto <cjprieto@uc.cl>
2022-01-27 10:24:19 -0300

    pagination first idea

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -4,20 +4,33 @@ import React, { FC, useState, useEffect } from "react";
 
 const App: FC = () => {
 
-  const baseURL = 'https://hn.algolia.com/api/v1/search_by_date?query=angular&page=0'
+  const baseURL = 'https://hn.algolia.com/api/v1/search_by_date'
 
   const [posts, setPosts] = useState<any[]>([]);
   const [newsType, setNewsType] = useState<string>('angular');
+  const [page, setPage] = useState<number>(0);
 
   useEffect(() => {
-    axios.get(`https://hn.algolia.com/api/v1/search_by_date?query=${newsType}&page=0`).then((response) => {
+    axios.get(`${baseURL}?query=${newsType}&page=${page}`).then((response) => {
       setPosts(response.data.hits);
     });
-  }, [newsType]);
+  }, [newsType, page]);
 
   const handleLike = () => {
     console.log('Liked')
   }
+
+  const nextPage = () => {
+    if (page !== 9) {
+      setPage(page + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (page !== 0) {
+      setPage(page - 1);
+    }
+  };
 
   return (
     <div className='container'>
@@ -37,16 +50,30 @@ const App: FC = () => {
 
         <div className='grid'>
           {posts.map((post, index) => (
-            <div className='grid-element'>
+            <div key={ index } className='grid-element'>
               <div className='content' onClick={ () => window.open(post.story_url, "_blank") }>
-                <span className='post-date'>{ post.created_at }</span>
+                <div className='date-container'>
+                  <img src="/iconmonstr-time-2.svg" alt=''/>
+                  <span className='post-date'>{ new Date(post.created_at).toString() }</span>
+                  <span className='post-author'>by { post.author }</span>
+                </div>
                 <span className='post-title'>{ post.story_title }</span>
               </div>
               <div className='like-button' onClick={ handleLike }>
-                <span>Like</span>
+                <img src="/iconmonstr-favorite-2.svg" alt=''/>
               </div>
             </div>
           ))}
+        </div>
+
+        <div className='pagination'>
+          <button className="btn next-btn" onClick={() => prevPage()}>
+            Previous
+          </button>
+          <span>{ page }</span>
+          <button className="btn next-btn" onClick={() => nextPage()}>
+            Next
+          </button>
         </div>
       </div>
     </div>
